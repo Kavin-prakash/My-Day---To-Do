7b00f25c2b29b21466f08d5503f35b87aaa3554e
Kavin Prakash Senthil Prabhu <kavinprakash1912@gmail.com>
2024-08-23 14:56:59 +0530

    changes : Implement notificaiton sound

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -17,6 +17,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
 import { Tooltip } from '@mui/material';
+import NotificationSound from '../Asserts/Sound.wav'
 export const List = () => {
     const [textdata, SetTextdata] = useState('');
     const [tasks, setTasks] = useState(() => {
@@ -48,16 +49,25 @@ export const List = () => {
         }
     };
 
+    // Handle notification audio
+
+    function play()
+    {
+        new Audio(NotificationSound).play();
+    }
+
     const handledeletelist = (id) => {
         const deleteUpdatedTask = tasks.filter(task => task.id !== id);
         setTasks(deleteUpdatedTask);
         localStorage.setItem('To-do-List Data', JSON.stringify(deleteUpdatedTask));
+        play();
     };
 
     const handleEdit = (listvalue, id) => {
         SetTextdata(listvalue);
         SetUpdatestate(true);
         SeteditId(id);
+        
     }
 
     const updateTask = () => {
@@ -83,6 +93,20 @@ export const List = () => {
             localStorage.setItem('To-do-List Data', JSON.stringify(finalTasks));
         }
     };
+    // Date only 
+    const dateobject = new Date();
+    const day = dateobject.getDay();
+    const month = dateobject.getMonth();
+    console.log(month)
+
+    const dayarray = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const dayString = dayarray[day];
+    console.log(dayString);
+    const monthsarray = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+        "octomber", "November", "December"];
+
+    const monthstring = monthsarray[month];
+    console.log(monthstring);
 
     return (
         <>
@@ -96,11 +120,16 @@ export const List = () => {
                             <Typography variant="h6" color="inherit" component="div">
                                 To<CheckIcon />Do
                             </Typography>
+                            {/* <Typography sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+                                {day},{monthstring}
+                            </Typography> */}
                         </Toolbar>
                     </AppBar>
                 </Box>
-
             </div>
+            <Row style={{ marginTop: '60px' }}>
+                <h2>{day},{monthstring}</h2>
+            </Row>
             <Container fluid id='todolist' className='todolist-container'>
                 <Row>
                     <Col xs={10} sm={6}>
